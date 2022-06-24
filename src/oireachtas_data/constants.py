b5cf621e4493fb3670c85d3f0beb558cf9188fbe
Robert Lucey <robert@glimpse.ie>
2022-06-24 18:00:51 +0100

    Add debates and members dir consts

--- src/oireachtas_data/constants.py
+++ src/oireachtas_data/constants.py
@@ -5,3 +5,13 @@ OIREACHTAS_DIR = os.path.join(
     '/opt/oireachtas_data',
     pkg_resources.require('oireachtas_data')[0].version.split('.')[0]
 )
+
+DEBATES_DIR = os.path.join(
+    OIREACHTAS_DIR,
+    'debates'
+)
+
+MEMBERS_DIR = os.path.join(
+    OIREACHTAS_DIR,
+    'members'
+)
