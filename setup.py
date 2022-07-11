65cb2cdb8203340c4b5031706b8adb9be0d5584d
Robert Lucey <robert@glimpse.ie>
2022-07-11 21:20:27 +0100

    Bump

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name='oireachtas-data',
-    version='0.1.5',
+    version='0.1.6',
     python_requires='>=3.6',
     description='Oireachtas debate data',
     long_description='Oireachtas debate data',
